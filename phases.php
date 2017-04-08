2c42984e703cfdb623fea08b053045309aff8607
Unknown <vanessakurtxx@gmail.com>
2017-04-07 21:43:38 -0400

    phases

--- phases.php
+++ phases.php
@@ -66,10 +66,48 @@ $projectid = $_GET["projectid"];
 		        <li><a href="#">Contact</a></li>
 		        <li><a href="index.php">Logout</a></li>
 		</ul>
-		<div id="wrapper">
-			<div id="projectBox">
-				<h1>je veux des phases</h1>
-			</div>
+
+		<div id="wrapper"> 
+					<h1>Phases for the project : <?php echo $projectid;?></h1>
+					<?php 
+						$conn = new mysqli($servername, $username, $password, $dbname);  
+						$sql = "SELECT * FROM phases WHERE project_id ='$projectid' ";
+						$result = $conn->query($sql); 
+
+						while ($row=mysqli_fetch_array($result)) 
+						{ 
+							?><div id="projectBoxPhases"><?php
+
+							?><label>Phase ID</label><p><?php echo $row['phase_id']; ?></p><?php
+							?><label>Status</label><p><?php echo $row['status'];?></p><?php
+							?><label>Start Date</label><p><?php echo $row['status'];?></p><?php
+							?><label>Complete Date</label><p><?php echo $row['complete_date'];?></p><?php
+							?><label>Time Needed</label><p><?php echo $row['time_needed'];?></p><?php
+							?><label>Budget</label><p><?php echo $row['budget'];?></p><?php
+							?><label>Actual Cost</label><p><?php echo $row['actual_cost'];?></p><?php
+							
+							?>	
+							<div class="action_btns">
+							<?php	 
+
+							if($_SESSION['privilege'] == 'Company'){
+							?>
+							  <input type='button' class="edit_button" id="edit_button<?php echo $row['project_id'];?>" value="edit" onclick="edit_row('<?php echo $row['project_id'];?>');">
+						 	  <input type='button' style="display: none;" class="save_button" id="save_button<?php echo $row['project_id'];?>" value="save" onclick="save_row('<?php echo $row['project_id'];?>');">
+							<?php 
+							} 
+							
+							?></div><?php								
+
+							?></div><?php	
+
+						}
+						
+						$conn->close();
+
+
+					?>
+
 		</div>
 </body>
 </html>
