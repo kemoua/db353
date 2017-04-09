a63054d1cbfa0ee7428f465577ea2971f2abd491
Unknown <vanessakurtxx@gmail.com>
2017-04-08 20:10:59 -0400

    ddd

--- tasks.php
+++ tasks.php
@@ -13,23 +13,6 @@ $projectid = $_GET["projectid"];
 <html>
 <head>
 	<link rel="stylesheet" href="css/styleHome.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="js/modify_tasks.js"></script>
-	<script type='text/javascript'>
-	$(document).ready(function(){ 
-	    $(window).scroll(function(){ 
-	        if ($(this).scrollTop() > 100) { 
-	            $('#scroll').fadeIn(); 
-	        } else { 
-	            $('#scroll').fadeOut(); 
-	        } 
-	    }); 
-	    $('#scroll').click(function(){ 
-	        $("html, body").animate({ scrollTop: 0 }, 600); 
-	        return false; 
-	    }); 
-	});
-	</script>
 	<title>TASKS</title>
 </head>
 <body>
@@ -50,7 +33,7 @@ $projectid = $_GET["projectid"];
 										$result2 = $conn2->query($sql1);
 										//query for users with no admin privileges
 										$user = $_SESSION['user'];
-										$sql2 = "SELECT * FROM projects,clients WHERE projects.client_id = clients.client_id AND clients.username ='$user' ";
+										$sql2 = "SELECT * FROM projects,client WHERE project.client_id = client.client_id AND client.username ='$user' ";
 										$resultClient = $conn2->query($sql2);
 										if($_SESSION['privilege'] == 'Company'){
 											while ($row=mysqli_fetch_array($result2)) 
@@ -76,10 +59,7 @@ $projectid = $_GET["projectid"];
 
 								</ul>
 		                        </li>
-		                        <?php
-		                        if($_SESSION['privilege'] == 'Company'){
-		                        	?><li><a href="create.php">Create</a></li>
-		                        <?php }?>	
+		                        <li><a href="#">Create</a></li>
 		                </ul>
 		        </li>
 		        <li><a href="#">About</a></li>
@@ -104,27 +84,26 @@ $projectid = $_GET["projectid"];
 						}
 					?>		
 
+					<div id="newTask">
+						<div id="projectBoxTasks">
+							<form action="">
+								<label>Task ID</label><input type="text" name="task_id">
+								<label>Project Id</label><input type="text" name="project_id">
 								<label>Phase ID</label><input type="text" name="phase_id">
-
-					<div id="newPhase">
-						<div id="projectBoxPhases">
-								<!-- <label>Task ID</label><input type="text" name="new_task_id"> -->
-								<label>Project Id</label><input type="text" name="new_project_id" value="<?php echo $projectid;?>" disabled>
-								<label>Phase ID</label><input type="text" name="new_phase_id">
-								<label>Description</label><input type="text" name="new_description">
-								<label>Status</label><input type="text" name="new_status">
-								<label>Start Date</label><p><input type="text" name="new_start_date">
-								<label>Complete Date</label><input type="text" name="new_complete_date">
-								<label>Time Needed</label><input type="text" name="new_time_needed">
-								<label>Budget</label><p><input type="text" name="new_budget">
-								<label>Cost</label><input type="text" name="new_actual_cost">								
-								<input type="button" value="Create Task" onclick="create_task();">
+								<label>Description</label><input type="text" name="description">
+								<label>Status</label><input type="text" name="status">
+								<label>Start Date</label><p><input type="text" name="start_date">
+								<label>Complete Date</label><input type="text" name="complete_date">
+								<label>Time Needed</label><input type="text" name="time_needed">
+								<label>Budget</label><p><input type="text" name="budget">
+								<label>Cost</label><input type="text" name="actual_cost">								
+								<input type="submit" value="Create">
 							</form>
 						</div> 
 					</div>
 					<?php 
 						$conn = new mysqli($servername, $username, $password, $dbname);  
-						$sql = "SELECT tasks.project_id,task_id,phases.status as phases_id,tasks.description,tasks.status as status,tasks.start_date,tasks.complete_date,tasks.time_needed,tasks.budget,tasks.cost FROM tasks,phases WHERE tasks.project_id ='$projectid' ";
+						$sql = "SELECT * FROM tasks WHERE project_id ='$projectid' ";
 						$result = $conn->query($sql); 
 
 						$num_rows = $result->num_rows;
@@ -135,17 +114,17 @@ $projectid = $_GET["projectid"];
 
 						while ($row=mysqli_fetch_array($result)) 
 						{ 
-							?><div id="projectBoxPhases"><?php
-
-							?><b>Task Id: </b><label id="task_id_val<?php echo $row['project_id'];?>"><?php echo $row['task_id'];?></label><br><?php 
-							?><b>Phase Status: </b><label id="phase_id_val<?php echo $row['project_id'];?>"><?php echo $row['phases_id'];?></label><br><?php 
-							?><b>Description: </b><label id="description_val<?php echo $row['project_id'];?>"><?php echo $row['description'];?></label><br><?php 
-							?><b>Status: </b><label id="status_val<?php echo $row['project_id'];?>"><?php echo $row['status'];?></label><br><?php 
-							?><b>Start date: </b><label id="start_date_val<?php echo $row['project_id'];?>"><?php echo $row['start_date'];?></label><br><?php 
-							?><b>Complete date: </b><label id="complete_date_val<?php echo $row['project_id'];?>"><?php echo $row['complete_date'];?></label><br><?php 
-							?><b>Time needed: </b><label id="time_needed_val<?php echo $row['project_id'];?>"><?php echo $row['time_needed'];?></label><br><?php 
-							?><b>Budget: </b><label id="budget_val<?php echo $row['project_id'];?>"><?php echo $row['budget'];?></label><br><?php 
-							?><b>Cost: </b><label id="cost_val<?php echo $row['project_id'];?>"><?php echo $row['cost'];?></label><br><?php 
+							?><div id="projectBoxTasks"><?php
+							?><h2>Task ID :<?php echo $row['task_id']; ?></h2><?php
+							?><label>Project ID</label><p><?php echo $row['project_id'];?></p><?php
+							?><label>Phase ID</label><p><?php echo $row['phase_id'];?></p><?php
+							?><label>Description</label><p><?php echo $row['description'];?></p><?php
+							?><label>Status</label><p><?php echo $row['status'];?></p><?php
+							?><label>Start Date</label><p><?php echo $row['start_date'];?></p><?php
+							?><label>Complete Date</label><p><?php echo $row['complete_date'];?></p><?php
+							?><label>Time Needed</label><p><?php echo $row['time_needed'];?></p><?php
+							?><label>Budget</label><p><?php echo $row['budget'];?></p><?php
+							?><label>Cost</label><p><?php echo $row['cost'];?></p><?php
 						
 							?>	
 							<div class="action_btns">
@@ -153,9 +132,9 @@ $projectid = $_GET["projectid"];
 
 							if($_SESSION['privilege'] == 'Company'){
 							?>
-							  <input type='button' class="edit_button" id="edit_button<?php echo $row['project_id'];?>" value="edit" onclick="edit_task('<?php echo $row['project_id'];?>');">
-						 	  <input type='button' style="display: none;" class="save_button" id="save_button<?php echo $row['project_id'];?>" value="save" onclick="save_task('<?php echo $row['project_id'];?>');">
-						 	  <input type='button' style="display: none;" class="save_button" id="cancel_button<?php echo $row['project_id'];?>" value="Cancel" onclick="cancel_edit_task('<?php echo $row['project_id'];?>');">
+							  <input type='button' class="deleteTask" >
+							  <input type='button' class="edit_button" id="edit_button<?php echo $row['project_id'];?>" value="" onclick="edit_row('<?php echo $row['project_id'];?>');">
+						 	  <input type='button' style="display: none;" class="save_button" id="save_button<?php echo $row['project_id'];?>" value="save" onclick="save_row('<?php echo $row['project_id'];?>');">
 							<?php 
 							} 
 							
